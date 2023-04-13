4c65829a65070d32e63c5bf78739dc49c15747ff
lifangjun <lifj@ebondhm.com>
2023-04-13 15:15:48 +0800

    feat(es): change README.md

--- ESCRUD/src/Main.java
+++ ESCRUD/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("测试 ES");
+        System.out.println("我是 MASTER 分支");
+        System.out.println("master: Hello ES");
     }
 }
